66b9d27ff18de6b5837d1dffd7305ba4f16a0efa
spitfire233 <87020972+spitfire233@users.noreply.github.com>
2026-08-03 16:37:52 +0200

    Started implementing dataset analysis script

---
+++ dataset_analysis.py
@@ -0,0 +1,45 @@
+"""
+This file contains a script that generates all relevant information and graphs
+for performing a exploratory analysis of both the ILVSRC validation split and
+ImageNet-R test split
+"""
+
+import os
+from datasets import load_dataset
+import logging
+
+logger = logging.getLogger(__name__)
+
+def main():
+    logging.basicConfig(filename="dataset_analysis.log", level=logging.INFO)
+    os.makedirs("./dataset_info", exist_ok= True)
+    # Load datasets with hugging face
+    # NOTE: this requires an hugging face account to log
+    # in by CLI with hf auth login, since the imagenet-1k dataset
+    # is gated by the organizers of the challenge
+    logger.info("Loading imagenet-r from huggingface")
+    
+    try:
+        imagenet_r_dataset = load_dataset("axiong/imagenet-r", split="test")
+    except Exception as e:
+        logger.error("Failed to load the imagenet-r dataset!")
+        print(e)
+    
+    logger.info("Successfully loaded imagenet-r")
+    logger.info("Loading imagenet-1k from huggingface")
+    
+    try:
+        imagenet_1k_dataset = load_dataset("ILSVRC/imagenet-1k", split="validation")
+    except Exception as e:
+        logger.error("Failed to load imagenet-1k. Have you logged in from CLI with hf auth login?")
+        print(e)
+        
+    
+    
+if __name__ == '__main__':
+    main()
+
+
+
+
+
