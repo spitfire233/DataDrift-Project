c49ae5193e777a334a551d07cc09ba62b9c37392
spitfire233 <87020972+spitfire233@users.noreply.github.com>
2026-08-03 22:16:01 +0200

    Fixed the dataset analysis script so now it really downloads only the validation split from the imagenet-1k dataset

--- dataset_analysis.py
+++ dataset_analysis.py
@@ -20,6 +20,7 @@ def main():
     logger.info("Loading imagenet-r from huggingface")
     
     try:
+        # Download the imagenet_r dataset with the provided dataset builder
         imagenet_r_dataset = load_dataset("axiong/imagenet-r", split="test")
     except Exception as e:
         logger.error("Failed to load the imagenet-r dataset!")
@@ -29,7 +30,15 @@ def main():
     logger.info("Loading imagenet-1k from huggingface")
      
     try:
-        imagenet_1k_dataset = load_dataset("ILSVRC/imagenet-1k", data_files={"validation": "data/validation-*"}, split="validation")
+        # Download only the validation split of the imagenet-1k.
+        # To do this, we have to avoid using the dataset builder provided by the challenge
+        # and instead use the built-in parquet builder
+        imagenet_1k_dataset = load_dataset(
+            "parquet",
+            data_files={
+                "validation": "hf://datasets/ILSVRC/imagenet-1k@refs/convert/parquet/default/validation/*.parquet"
+            },
+            split="validation")
     except Exception as e:
         logger.error("Failed to load imagenet-1k. Have you logged in from CLI with hf auth login?")
         print(e)
@@ -38,8 +47,3 @@ def main():
     
 if __name__ == '__main__':
     main()
-
-
-
-
-
